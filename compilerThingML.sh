654e661fd1d0935ddb945be737ca21fcb2b192c7
Alexandre Rio <contact@alexrio.fr>
2016-03-23 11:47:39 +0100

    Add compilerThingML wrapper

---
+++ compilerThingML.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Description:
+#     This script is a wrapper for the ThingML compiler. You can get it
+#  and compile it at: https://github.com/SINTEF-9012/ThingML
+#
+# Author:
+# - Alexandre RIO <contact@alexrio.fr>
+#
+# Dependencies:
+# - Java JRE Version 7 or better
+#
+############################################################################
+
+THINGML_JAR=$HOME/.bin/compilerThingML.jar
+THINGML_PLUGINS=$HOME/.bin/thingml-network.jar:
+
+java -cp $CLASSPATH:$THINGML_JAR:$THINGML_PLUGINS org.thingml.compilers.commandline.Main "$@"
